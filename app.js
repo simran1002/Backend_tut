8770487b3afb1962c82e6c37506425a3d5b99893
Simran Yadav <simranyadav464@gmail.com>
2023-07-19 02:09:15 +0530

    Updated content

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
+const path = require('path');
 const http = require('http');
-
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop');
+const { nextTick } = require('process');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/admin', adminRoutes); 
+app.use(shopRoutes);
 // const routes = require('./routes_old');
 app.use((req, res, next) => {
-    console.log('In the middleware!')
-    next();//Allows the request to continue to the next middleware in line
-});
-app.use((req, res, next) => {
-    console.log('In another middleware!')
-    res.send('<h1>Hello from Express!</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
 
 // const server = http.createServer(app);
-// server.listen(3000);
+// server.listen(3000);  
