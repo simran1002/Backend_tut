10b5231dcfa25ada40212d98ae4e4624c45dbab6
Simran Yadav <simranyadav464@gmail.com>
2023-07-18 23:39:00 +0530

    Adding files

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-          console.log(req);
-          //process.exit();
+const express = require('express');
+
+const app = express();
+
+// const routes = require('./routes_old');
+app.use((req, res, next) => {
+    console.log('In the middleware!')
+    next();//Allows the request to continue to the next middleware in line
+});
+app.use((req, res, next) => {
+    console.log('In another middleware!')
+    res.send('<h1>Hello from Express!</h1>');
 });
 
-server.listen(3000);
+app.listen(3000);
+
+// const server = http.createServer(app);
+// server.listen(3000);
