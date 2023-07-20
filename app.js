306bd819f69f9ac62fa509db67c9b055c88c6725
Simran Yadav <simranyadav464@gmail.com>
2023-07-21 03:46:43 +0530

    Updated files

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const path = require('path');
-const http = require('http');
+const path = require('./util/path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express();
 
-const adminRoutes = require('./routes/admin')
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const { nextTick } = require('process');
-app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use('/admin', adminRoutes); 
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
-// const routes = require('./routes_old');
+
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
-
-// const server = http.createServer(app);
-// server.listen(3000);  
--- routes/admin.js
+++ routes/admin.js
@@ -6,15 +6,24 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
-//  /admin/add-product => GET 
+const products = [];
+
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootdir, 'views', 'add-product.html'));
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 });
 
-//  /admin/add-product => POST
-router.post('/add-product', (req,res,next) => {
-    console.log(req.body);
-    res.redirect('/');
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
